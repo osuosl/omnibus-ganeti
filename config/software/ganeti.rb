0175e1c33f805398f05e12796488ac9c6d6d4773
Lance Albertson <lance@osuosl.org>
2014-05-04 23:22:06 -0700

    Fix installation to use symlinks
    For some reason it doesn't work otherwise.
    
    Also fix the python path for a few files since they directly use
    /usr/bin/python.
    
    Add support for other major versions of ganeti

--- config/software/ganeti.rb
+++ config/software/ganeti.rb
@@ -1,8 +1,23 @@
 name "ganeti"
 default_version "2.11.0"
 
-source :url => "http://downloads.ganeti.org/releases/2.11/ganeti-#{default_version}.tar.gz",
-       :md5 => "2c186d48b56b7d4083161c241f91febe"
+version "2.11.0" do
+  source md5: "2c186d48b56b7d4083161c241f91febe"
+end
+
+version "2.10.3" do
+  source md5: "7c59fc1be2188d248739a829b5deb002"
+end
+
+version "2.9.6" do
+  source md5: "ff2943bc3dc38d03f3a4ffbcf3b31b4e"
+end
+
+version "2.8.4" do
+  source md5: "6d9ddb5a9645a3d57a4f90dd92677bcc"
+end
+
+source url: "http://downloads.ganeti.org/releases/#{default_version[/\d.\d+/]}/ganeti-#{default_version}.tar.gz"
 
 relative_path "ganeti-#{default_version}"
 
@@ -11,11 +26,13 @@ dependency 'ganeti-common'
 
 configure = ["./configure",
            "--prefix=#{install_dir}/embedded",
+           "--enable-symlinks",
            "--localstatedir=#{install_dir}/var",
            "--sysconfdir=#{install_dir}/etc"].join(" ")
 
 sed = ["sed -i -e 's/\\!\\/usr/\\!\\/opt\\/ganeti\\/embedded/'",
-       "autotools/build-bash-completion"].join(" ")
+       "autotools/build-bash-completion",
+       "Makefile"].join(" ")
 
 env = {
   "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
