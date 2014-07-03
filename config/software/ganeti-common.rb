d46dc91bd5573fae44600bec5e8f75c287572d46
Lance Albertson <lance@osuosl.org>
2014-07-03 08:30:51 -0700

    Build ndisc/socat before python/haskell deps
    If we have failures in python or haskell deps, lets not rebuild these.

--- config/software/ganeti-common.rb
+++ config/software/ganeti-common.rb
@@ -1,6 +1,6 @@
 name "ganeti-common"
 
-dependency 'ganeti-common-python'
-dependency 'ganeti-common-haskell'
 dependency 'ndisc6'
 dependency 'socat'
+dependency 'ganeti-common-python'
+dependency 'ganeti-common-haskell'
